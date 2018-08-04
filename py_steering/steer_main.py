7b7c482a260508229e714af747d54826826e7f56
Adam Spannbauer <SpannbauerAdam@gmail.com>
2018-08-04 10:17:22 -0700

    add pyimagesearch logo

--- py_steering/steer_main.py
+++ py_steering/steer_main.py
@@ -1,14 +1,15 @@
 import argparse
 import cv2
+import imutils
 from utils import steer_image
 
 ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--input',
-                # default='python_logo.png',
-                # default='opencv_logo.png',
-                default='dual_logo.png',
+ap.add_argument('-i', '--input', default='pyimage_combo.png',
                 help='path to input image')
+ap.add_argument('-w', '--resizeWidth', default=900,
+                help='pixel width to resize to before processing')
 args = vars(ap.parse_args())
 
 image = cv2.imread(args['input'])
+image = imutils.resize(image, width=args['resizeWidth'])
 steer_image(image)
